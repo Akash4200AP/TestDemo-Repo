04129af6570797fcfd9cea0a01e7562210c2e7d9
Akash <akashpattiloffi@gmail.com>
2025-05-22 17:58:13 +0530

    This is my second comment

---
+++ src/constructormethod/defualtconstructor1.java
@@ -0,0 +1,17 @@
+package constructormethod;
+
+public class defualtconstructor1 {
+	
+	defualtconstructor1()
+	{
+		
+		System.out.println("This is default constructor");
+		
+	}
+
+	public static void main(String[] args) {
+		
+		defualtconstructor1 c=new defualtconstructor1();
+	}
+
+}
